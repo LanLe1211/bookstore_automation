80e238b8f24942028781aa7934d930d98670c131
LanLe <lanltfx01326@funix.edu.vn>
2024-12-23 08:49:30 +0700

    Allow chrome to run as root user

--- src/main/java/com/cucumberFramework/testBase/TestBase.java
+++ src/main/java/com/cucumberFramework/testBase/TestBase.java
@@ -2,14 +2,12 @@ package com.cucumberFramework.testBase;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.cucumberFramework.enums.Browsers;
 import com.cucumberFramework.enums.OS;
 import com.cucumberFramework.helper.wait.WaitHelper;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 
 public class TestBase {
 
@@ -21,7 +19,10 @@ public class TestBase {
 		if (System.getProperty("os.name").toLowerCase().contains(OS.LINUX.name().toLowerCase())) {
 			if (browser.equalsIgnoreCase(Browsers.CHROME.name())) {
 				//System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-				driver = new ChromeDriver();
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--no-sandbox");
+		        options.addArguments("--disable-dev-shm-usage");
+				driver = new ChromeDriver(options);
 
 			} else if (browser.equalsIgnoreCase(Browsers.FIREFOX.name())) {
 				//System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver");
