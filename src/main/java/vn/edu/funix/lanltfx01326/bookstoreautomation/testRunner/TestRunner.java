de76a86ce63b294837297ef781bd5e11e27cd0b9
Lan Le <lanltfx01326@funix.edu.vn>
2025-06-09 23:18:51 -0400

    Update test report to cucumber report

--- src/main/java/vn/edu/funix/lanltfx01326/bookstoreautomation/testRunner/TestRunner.java
+++ src/main/java/vn/edu/funix/lanltfx01326/bookstoreautomation/testRunner/TestRunner.java
@@ -1,53 +1,35 @@
 package vn.edu.funix.lanltfx01326.bookstoreautomation.testRunner;
 
-import java.lang.reflect.Method;
-
 import org.openqa.selenium.WebDriver;
-import org.testng.ITest;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import vn.edu.funix.lanltfx01326.bookstoreautomation.testBase.TestBase;
 
-@CucumberOptions(
-	features = "src/test/resources/OnlineBookstoreFeatures/login_admin.feature", 
-	glue = {"vn/edu/funix/lanltfx01326/bookstoreautomation/stepDefinitions" },
-	plugin = { 
-		"pretty",
-		"html:target/cucumber-html-reports/report.html",
-		"json:target/cucumber-reports/CucumberTestReport.json"
-	}
-)
-public class TestRunner extends AbstractTestNGCucumberTests implements ITest{
-	private ThreadLocal<String> testName = new ThreadLocal<>();
-	
-    @BeforeClass(alwaysRun = true)
-    @Override
-    public void setUpClass(ITestContext context) {
-    	super.setUpClass(context);
-    	TestBase.initDriverForBrowser("chrome");
-    }
-    
-    @AfterClass(alwaysRun = true)
-    @Override
-    public void tearDownClass() {
-    	WebDriver driver = TestBase.getCurrentWebDriver();
-    	if (driver != null) {
-    		driver.close();
-    	}
-    }
-    
-    @BeforeMethod
-    public void BeforeMethod(Method method, Object[] testData){
-        testName.set(String.valueOf(testData[0]));
-    }
+@CucumberOptions(features = "src/test/resources/OnlineBookstoreFeatures/login_admin.feature", glue = {
+		"vn/edu/funix/lanltfx01326/bookstoreautomation/stepDefinitions" }, plugin = { "pretty",
+				"html:target/cucumber-html-reports/report.html",
+				"json:target/cucumber-reports/CucumberTestReport.json" })
+public class TestRunner extends AbstractTestNGCucumberTests {
 
+	@BeforeClass(alwaysRun = true)
 	@Override
-	public String getTestName() {
-		return testName.get();
+	public void setUpClass(ITestContext context) {
+		super.setUpClass(context);
+		TestBase.initDriverForBrowser("chrome");
 	}
+
+	@AfterClass(alwaysRun = true)
+	@Override
+	public void tearDownClass() {
+		super.tearDownClass();
+		WebDriver driver = TestBase.getCurrentWebDriver();
+		if (driver != null) {
+			driver.close();
+		}
+	}
+
 }
